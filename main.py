11229e82f6ba88e03bc0f5a1a1205ee92b9907ff
acey <mofe.obasola@gmail.com>
2024-07-16 19:18:11 +0100

    Created a database parser

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 import time
+import json
+
 command = "cls"
 database= "db.txt"
+
+with open(database, "r") as f:
+    lines = f.read()
+
+data = json.loads(lines)
+
+
 def menu():
-    os.system(command)
+    #os.system(command)
     option = input("What would you like to do?\n===============================\n[1] View a ToDo List\n[2] Edit one of your ToDo Lists\n[3] Create / Delete a ToDo List\n===============================\n-> ")
 
-menu()
